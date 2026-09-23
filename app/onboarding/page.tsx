f4837f7de2b62df711b58b1c79a9507703a5e2a0
deka-ab <dekabulhan@gmail.com>
2026-09-23 18:10:20 +0200

    lagt til komponenter - knapp osv

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,19 +1,24 @@
+import Button from "@/components/Button"; 
+
 export default function Onboarding() {
+    
     return (
         <main>
-            <h1> Tell us about yourself</h1>
+            <h1 className="text-4xl font-bold"> Tell us about yourself</h1>
 
-            <h2> What are you interested in?</h2>
+            <h2 className = "text-lg "> What are you interested in?</h2>
 
-            <button> Gaming</button>
+            <Button text="Gaming" />
 
-            <button>Psychology </button>
+            <button className="bg-green-500 text-white px-4 py-2 rounded">Psychology </button>
 
-            <button> Technology </button>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded"> Technology </button>
 
-            <button> Sport </button>
+            <button className="bg-red-500 text-white px-4 py-2 rounded"> Sport </button>
 
-            <button> History </button>
+            <button className="bg-purple-500 text-white px-4 py-2 rounded"> History </button>
+
+            <p className="text-lg">  Continue</p>
         
         </main>
     );
